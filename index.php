26814f336e62783002bac0cb3b83744cbb2b827f
MKA1500 <maciejkabsch@gmail.com>
2018-10-10 11:53:36 +0200

    layout works

--- index.php
+++ index.php
@@ -1,23 +1,44 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="styles_dist/style.css" />
   <title>My Online Shop</title>
 </head>
 <body>
-  <header>Header</header>
-  <div class="menu-bar">
-    menu-bar
-  </div>
+  <header class="header">
+    <div class="brand">
+      <h1 class="shop-title">agro-polo</h1>
+    </div>
+
+    <div class="menu-bar">
+
+      <ul class="menu-list">
+        <li class="menu-item"><a href="#" class="menu-link">Home</a></li>
+        <li class="menu-item"><a href="#" class="menu-link">All Products</a></li>
+        <li class="menu-item"><a href="#" class="menu-link">My Account</a></li>
+        <li class="menu-item"><a href="#" class="menu-link">Sign Up</a></li>
+        <li class="menu-item"><a href="#" class="menu-link">Shopping Cert</a></li>
+        <li class="menu-item"><a href="#" class="menu-link">Contact Us</a></li>
+      </ul>
+
+      <form class="form-inline">
+          <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+          <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+      </form>
+
+    </div>
+  </header>
+
   <div class="sidebar">
     sidebar
   </div>
-  <main class="container">
+  <main class="main">
     main content
   </main>
-  <footer>
+  <footer class="footer">
     footer
   </footer>
-  <p>Hello my customers!</p>
 </body>
 </html>
